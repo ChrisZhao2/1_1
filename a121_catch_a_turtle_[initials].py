61ea7842a977561b6e4262f94c1611313315e6bc
ChrisZhao2 <91628314+ChrisZhao2@users.noreply.github.com>
2021-11-03 11:06:37 -0700

    Add files via upload

--- a121_catch_a_turtle_[initials].py
+++ a121_catch_a_turtle_[initials].py
@@ -2,18 +2,33 @@
 #-----import statements-----
 import turtle as trtl
 spot = trtl.Turtle()
-
+import random as rand
 #-----game configuration----
-spot.fillcolor("red")
-spot.begin_fill()
-spot.circle(50)
-spot.end_fill()
+spotshape="circle"
+shapesize=10
+spotcolor="red"
+spot.shape()
 
 #-----initialize turtle-----
-
+spot.shape(spotshape)
+spot.turtlesize(shapesize)
+spot.fillcolor(spotcolor)
+spot.speed(0)
+spot.penup()
 #-----game functions--------
 def spot_clicked(x,y):
-  print("Spot was clicked!")
+  size=rand.randint(1,10)
+  change_position(size)
+def change_position(size):
+  new_xpos = rand.randint(0,400)
+  new_ypos = rand.randint(0,300)
+  colors = ["blue" , "red", "green", "pink"]
+  colorcycle = rand.randint(0,3)
+  spot.hideturtle()
+  spot.fillcolor(colors[colorcycle])
+  spot.turtlesize(size)
+  spot.goto(new_xpos,new_ypos)
+  spot.showturtle()
 
 #-----events----------------
 spot.onclick(spot_clicked)
